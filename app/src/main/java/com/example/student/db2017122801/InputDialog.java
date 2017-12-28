639f38c40ad3f0dd75f83abac620dad6e1f1f42b
Shaojung <shaojung@gmail.com>
2017-12-28 19:23:33 +0800

    Input dialog use DialogFragment

--- app/src/main/java/com/example/student/db2017122801/InputDialog.java
+++ app/src/main/java/com/example/student/db2017122801/InputDialog.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by student on 2017/12/28.
@@ -21,7 +23,20 @@ public class InputDialog extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = getActivity().getLayoutInflater().inflate(R.layout.myinputdialog, null);
+        Button btn = view.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Click", Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
         // return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
     }
 }
